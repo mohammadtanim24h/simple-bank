5e20847c9a3ebd99bac6f86c15a02d1caa1a5bff
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-02-10 21:52:59 +0600

    deposit section added

--- js/banking.js
+++ js/banking.js
@@ -1,8 +1,14 @@
 
 // deposit
 
-document.getElementById('deposit-button').addEventListener('click', function (e) {
+document.getElementById('deposit-button').addEventListener('click', function () {
+    // get the amount deposited
     const depositField = document.getElementById('deposit-field');
-    const depositAmount = depositField.value;
-    console.log(depositAmount);
+    const newDepositAmount = depositField.value;
+    const depositTotal = document.getElementById('deposit-total');
+    const previousDepositAmount = depositTotal.innerText;
+    const newDepositTotal = parseFloat(previousDepositAmount) + parseFloat(newDepositAmount);
+    depositTotal.innerText = newDepositTotal;
+    // clear deposit field value
+    depositField.value = '';
 })
