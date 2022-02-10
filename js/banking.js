d51fc42d15c0f198ba829b2d1d412e33ee0b0523
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-02-11 00:33:45 +0600

    withdraw section added

--- js/banking.js
+++ js/banking.js
@@ -1,5 +1,8 @@
 
 // deposit
+// get the amount deposited er explanation
+// better explained in the video 7 and 8. so i better watch them for explanation
+// // deposit-button e click korle jate input er value ta ashe eijonno ekhane deposit-field re id dia dhoira nia aisha depositField namer variable declare korsi. jehetu deposit-field er input field e ami jeta input dicchi oita new deposit tai depositField er value ke niye newDepositAmount namer ekta variable declare korsi. tarpor deposit-total ke id dia nia aisha depositTotal e declare korsi. karon eitai amar total deposit. Prottekbar new deposit add korle ei total deposit tai previous deposit hoye jabe tai previousDepositAmount er moddhe depositTotal.innertext assign korsi. lastly newDepositTotal namer ekta variable declare korsi jeitar value holo previousDepositAmount + newDepositAmount. and eigula jehetu string tai eigula ke number e convert korsi parseFloat diye and jug korsi. ei depositTotal.innerText = newDepositTotal diye depositTotal er value update kore dicchi. 
 
 document.getElementById('deposit-button').addEventListener('click', function () {
     // get the amount deposited
@@ -9,6 +12,38 @@ document.getElementById('deposit-button').addEventListener('click', function () 
     const previousDepositAmount = depositTotal.innerText;
     const newDepositTotal = parseFloat(previousDepositAmount) + parseFloat(newDepositAmount);
     depositTotal.innerText = newDepositTotal;
+
+    // update account balance 
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceAmount = balanceTotal.innerText;
+    const newBalanceTotal = parseFloat(previousBalanceAmount) + parseFloat(newDepositAmount);
+    balanceTotal.innerText = newBalanceTotal;
     // clear deposit field value
     depositField.value = '';
+});
+
+// withdraw
+// withdraw tao almost same as deposit
+
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    const withdrawField = document.getElementById('withdraw-field');
+    // empty input error handling
+    // if (withdrawField.value == '') {
+    //     alert('please enter a number');
+    //     return;
+    // }
+    const newWithdraw = withdrawField.value;
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdraw = withdrawTotal.innerText;
+    const newWithdrawTotal = parseFloat(previousWithdraw) + parseFloat(newWithdraw);
+    withdrawTotal.innerText = newWithdrawTotal;
+
+    // update account balance
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceAmount = balanceTotal.innerText;
+    const newBalanceTotal = parseFloat(previousBalanceAmount) - parseFloat(newWithdraw);
+    balanceTotal.innerText = newBalanceTotal;
+
+    // clear withdraw field value
+    withdrawField.value = '';
 })
